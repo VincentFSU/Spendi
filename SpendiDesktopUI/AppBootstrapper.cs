865e0ed343f13cd3524f35e6dac73b99afb2a318
Vince <paystaxes@protonmail.com>
2020-04-09 15:31:58 -0400

    Caliburn Micro configuration for MVVM
    Includes simple container DI

--- SpendiDesktopUI/AppBootstrapper.cs
+++ SpendiDesktopUI/AppBootstrapper.cs
@@ -1,19 +1,47 @@
 namespace SpendiDesktopUI {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using Caliburn.Micro;
     using SpendiDesktopUI.ViewModels;
 
     public class AppBootstrapper : BootstrapperBase 
     {
+        private SimpleContainer _container = new SimpleContainer();
+
         public AppBootstrapper() 
         {
             Initialize();
         }
 
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container.Singleton<IWindowManager, WindowManager>().Singleton<IEventAggregator, EventAggregator>();
+
+            //register each ViewModel to the container (reflection)
+            GetType().Assembly.GetTypes().Where(type => type.IsClass).Where(type => type.Name.EndsWith("ViewModel")).ToList().ForEach(viewModelType => _container.RegisterPerRequest(viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
         protected override void OnStartup(object sender, System.Windows.StartupEventArgs e) 
         {
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
