f47e30cc0a0f77241119d22371e0aa32d6aa37ed
Vince <paystaxes@protonmail.com>
2020-04-28 17:01:10 -0400

    Added Sales Page

---
+++ SpendiData/dbo/StoredProcedures/spUserLookup.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spUserLookup]
+	@Id int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT Id, FirstName, LastName
+	FROM [dbo].[User]
+	WHERE Id = @Id;
+END
---
+++ SpendiDesktopUI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,127 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendiDesktopUI.ViewModels
+{
+    public class SalesViewModel : Screen
+    {
+		private BindingList<string> _products;
+
+		public BindingList<string> Products
+		{
+			get { return _products; }
+			set 
+			{
+				_products = value;
+				NotifyOfPropertyChange(() => Products);
+			}
+		}
+
+		private BindingList<string> _cart;
+
+		public BindingList<string> Cart
+		{
+			get { return _cart; }
+			set 
+			{
+				_cart = value;
+				NotifyOfPropertyChange(() => Cart);
+			}
+		}
+
+
+		private string _itemQuantity;
+
+		public string ItemQuantity
+		{
+			get { return _itemQuantity; }
+			set 
+			{
+				_itemQuantity = value;
+				NotifyOfPropertyChange(() => ItemQuantity);
+			}
+		}
+
+		public string Subtotal
+		{
+			get
+			{
+				return "$0.00";
+			}
+		}
+
+		public string Tax
+		{
+			get
+			{
+				return "$0.00";
+			}
+		}
+
+		public string Total
+		{
+			get
+			{
+				return "$0.00";
+			}
+		}
+
+
+		public bool CanRemoveFromCart
+		{
+			get
+			{
+				bool output = false;
+
+
+
+				return output;
+			}
+		}
+
+		public void RemoveFromCart()
+		{
+
+		}
+
+		public bool CanAddToCart
+		{
+			get
+			{
+				bool output = false;
+
+				
+
+				return output;
+			}
+		}
+
+		public void AddToCart()
+		{
+
+		}
+
+		public bool CanCheckout
+		{
+			get
+			{
+				bool output = false;
+
+
+
+				return output;
+			}
+		}
+
+		public void Checkout()
+		{
+
+		}
+
+	}
+}
